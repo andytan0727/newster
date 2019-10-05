d0a38f963734e816831773cffab3ff2c410cac46
Andy Tan <andytan0727@gmail.com>
2019-10-05 22:41:57 +0800

    feat: add new Request interface
    Request interface acts as an standard interface for both tests and prod
    codes.

--- api/news/interfaces.go
+++ api/news/interfaces.go
@@ -1,6 +1,14 @@
 package news
 
+import "io"
+
 // Scraper defines a standard scrap function to all scrapers
 type Scraper interface {
 	Scrap() ([]News, error)
+}
+
+// Request defines a standard HTTP Request interface
+// for Scrapers that contains RequestData function
+type Request interface {
+	RequestData(string) (io.ReadCloser, error)
 }
