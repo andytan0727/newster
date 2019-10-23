610684b807ccfac3467694508dc8d069e8c68c22
Andy Tan <andytan0727@gmail.com>
2019-10-23 10:10:38 +0800

    refactor: update news title list item styles

--- frontend/src/components/NewsTitleListItem.vue
+++ frontend/src/components/NewsTitleListItem.vue
@@ -1,7 +1,8 @@
 <template>
   <li class="news-title-list-item">
     <a :href="url" rel="noopener noreferrer" target="_blank">
-      {{ idx + 1 }}. {{ title }}
+      <span class="item-idx"> {{ idx + 1 }}. </span>
+      <span class="title">{{ title }}</span>
     </a>
   </li>
 </template>
@@ -27,13 +28,51 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../_variables.scss";
+
 .news-title-list-item {
   display: block;
-  padding: 1rem 1rem;
   text-align: left;
 
-  &:not(:last-child) {
-    border-bottom: 2px solid black;
+  @media screen and (max-width: $mobile-width) {
+    & a {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .item-idx {
+    display: inline-block;
+    width: 30px;
+    margin-right: 1rem;
+    text-align: right;
+
+    @media screen and (max-width: $mobile-width) {
+      margin-left: -10px;
+      width: 30px;
+    }
+  }
+
+  .title {
+    display: inline-block;
+    padding: 1.5rem 0;
+    width: 95%;
+
+    &:not(&:last-child) {
+      border-bottom: 1px solid rgba($color: #000, $alpha: 0.5);
+    }
+
+    @media screen and (max-width: $large-display-width) {
+      width: 90%;
+    }
+
+    @media screen and (max-width: $mobile-width) {
+      font-size: 1rem;
+      width: 240px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
   }
 
   a {
